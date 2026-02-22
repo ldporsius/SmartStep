d463ebba7b5604ddf158be939fc609690d9789cb
ldporsius <linda.d.porsius@gmail.com>
2026-02-22 14:50:43 +0100

    weekly average

---
+++ app/src/test/java/nl/codingwithlinda/smartstep/features/weekly_average/presentation/WeeklyAverageViewModelTest.kt
@@ -0,0 +1,38 @@
+package nl.codingwithlinda.smartstep.features.weekly_average.presentation
+
+import androidx.core.util.toClosedRange
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.hasSize
+import kotlinx.coroutines.test.runTest
+import nl.codingwithlinda.smartstep.tests.FakeDailyStepRepo
+import nl.codingwithlinda.smartstep.util.BaseJunitTest
+import org.junit.Assert.*
+import org.junit.Test
+
+class WeeklyAverageViewModelTest : BaseJunitTest(){
+
+    val repo = FakeDailyStepRepo()
+    val vm = WeeklyAverageViewModel(repo)
+
+
+    @Test
+    fun `test past week - all seven days in range`()= runTest(testDispatcher) {
+        vm.pastWeek.onEach {
+            println("past day: $it")
+        }
+    }
+
+    @Test
+    fun `test weekly average viewmodel - flow of items`() = runTest {
+        vm.lastSevenStepCounts.test {
+            val em0 = awaitItem()
+            println("em0: $em0")
+            val em1 = awaitItem()
+            println("em1: $em1")
+            assertThat(em1).hasSize(7)
+        }
+    }
+
+
+}
