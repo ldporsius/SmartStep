a2216f7ec8cab4e8f76bf285807ddbf4cffd7d89
ldporsius <linda.d.porsius@gmail.com>
2026-02-26 14:41:31 +0100

    abstraction

---
+++ app/src/main/java/nl/codingwithlinda/smartstep/core/domain/step_tracker_finite_state/SmartStepStateControllerImpl.kt
@@ -0,0 +1,89 @@
+package nl.codingwithlinda.smartstep.core.domain.step_tracker_finite_state
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.PowerManager
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import nl.codingwithlinda.smartstep.core.presentation.util.permissionsPerBuild
+import nl.codingwithlinda.smartstep.core.domain.step_tracker_finite_state.concrete_states.BackgroundRunningAllowed
+import nl.codingwithlinda.smartstep.core.domain.step_tracker_finite_state.concrete_states.BackgroundRunningDenied
+import nl.codingwithlinda.smartstep.core.domain.step_tracker_finite_state.concrete_states.PermissionNeeded
+
+class SmartStepStateControllerImpl(
+    private val context: ComponentActivity,
+) : SmartStepStateController{
+    private val _state = MutableStateFlow<StartTrackingState>(
+        PermissionNeeded(context, emptyMap()))
+    val startTrackingState = _state.asStateFlow()
+
+
+    private val permissionLauncher = context.registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { resultMap ->
+        val allGranted = resultMap.all {
+            it.value
+        }
+        val notGranted = resultMap.filter {
+            it.value == false
+        }
+        if(!allGranted){
+            _state.update {
+                PermissionNeeded(
+                    context = context,
+                    neededPermissions = notGranted
+                )
+            }
+        }
+    }
+
+    override fun onResult(){
+        println("--- SMART STEP STATE CONTROLLER --- on result")
+        if(!hasPermissions()){
+            checkPermissions()
+            println("--- SMART STEP STATE CONTROLLER --- has no permissions")
+            return
+        }
+        when(isIgnoringBatteryOptimizations()){
+            true -> {
+                val state = BackgroundRunningAllowed(context)
+                _state.update { state }
+            }
+
+            false -> {
+                val state = BackgroundRunningDenied(context)
+                _state.update { state }
+            }
+        }
+    }
+
+    fun isIgnoringBatteryOptimizations(): Boolean {
+        val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        return powerManager.isIgnoringBatteryOptimizations(context.packageName)
+    }
+
+    private fun hasPermissions(): Boolean {
+        val hasPermissions = permissionsPerBuild(Build.VERSION.SDK_INT).map{
+             context.checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
+        }.all { granted ->
+            granted
+        }
+
+       return hasPermissions
+    }
+
+    private fun checkPermissions(){
+        permissionLauncher.launch(permissionsPerBuild(Build.VERSION.SDK_INT).toTypedArray())
+    }
+
+    companion object{
+        fun isIgnoringBattery(context: Context): Boolean{
+            val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+            return powerManager.isIgnoringBatteryOptimizations(context.packageName)
+        }
+    }
+}
