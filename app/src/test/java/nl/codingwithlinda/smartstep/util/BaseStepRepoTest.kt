257d6fce86283aec59c59ffee26877ceede94d21
ldporsius <linda.d.porsius@gmail.com>
2026-03-14 18:09:40 +0100

    refactor tests

---
+++ app/src/test/java/nl/codingwithlinda/smartstep/util/BaseStepRepoTest.kt
@@ -0,0 +1,38 @@
+package nl.codingwithlinda.smartstep.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import nl.codingwithlinda.smartstep.tests.FakeActivityRecognitionRepo
+import nl.codingwithlinda.smartstep.tests.FakeDailyStepRepo
+import org.junit.After
+import org.junit.Before
+
+@OptIn(ExperimentalCoroutinesApi::class)
+abstract class BaseStepRepoTest(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) {
+
+    val activityRecognitionRepo = FakeActivityRecognitionRepo()
+    val fakeDailyStepRepo = FakeDailyStepRepo(
+        stepsTaken = activityRecognitionRepo.stepCount,
+        getStepCountForDate = {
+            activityRecognitionRepo.getStepCountForDate(it)
+        }
+    )
+
+    @Before
+    open fun setup(){
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    open fun tearDown(){
+        activityRecognitionRepo.reset()
+        fakeDailyStepRepo.reset()
+        Dispatchers.resetMain()
+    }
+}
