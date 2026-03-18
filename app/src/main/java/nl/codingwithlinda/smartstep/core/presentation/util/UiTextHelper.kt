84b957b5c93c4b8a5d4b937c8cacc5164a98af38
ldporsius <linda.d.porsius@gmail.com>
2026-03-18 13:05:19 +0100

    cleanup

--- app/src/main/java/nl/codingwithlinda/smartstep/core/presentation/util/UiTextHelper.kt
+++
@@ -1,7 +0,0 @@
-package nl.codingwithlinda.smartstep.core.presentation.util
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import nl.codingwithlinda.smartstep.core.presentation.util.UiText
-import androidx.compose.ui.res.stringResource
-
